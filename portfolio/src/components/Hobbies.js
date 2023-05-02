110300d9cf58658c24743b3c5b99756d8d843edd
wnylund <willn@vt.edu>
2023-05-01 23:46:45 -0400

    img fixes

--- portfolio/src/components/Hobbies.js
+++ portfolio/src/components/Hobbies.js
@@ -12,10 +12,10 @@ function Hobbies() {
       <Row>
             <p>Two of my favorite things to do are play volleyball and lift weights!</p>
             <Col xs={12} md={6} className="d-flex justify-content-md-end align-items-center pb-3 pb-md-0">
-              <img src={`${process.env.PUBLIC_URL}/volley.jpg`} alt="Your Name" width="225" height="300"/>
+              <img src={`${process.env.PUBLIC_URL}/volley.JPG`} alt="Your Name" width="225" height="300"/>
             </Col>
             <Col xs={12} md={6} className="d-flex justify-content-md-end align-items-center pb-3 pb-md-0">
-              <img src={`${process.env.PUBLIC_URL}/weight.png`} alt="Your Name" width="225" height="300"/>
+              <img src={`${process.env.PUBLIC_URL}/weight.PNG`} alt="Your Name" width="225" height="300"/>
             </Col>
       </Row>
     </Container>
